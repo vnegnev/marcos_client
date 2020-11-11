2d9c76ba00df302d1c79d25a74fad0fc62957a6a
Vlad Negnevitsky <vee neg nev at gee mail dot com>
2020-11-11 21:17:31 +0100

    Loopback test mostly working

--- test_loopback.py
+++ test_loopback.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# ocra-pulseq test
+# loopback test using ocra-pulseq
 #
 
 import numpy as np
@@ -10,33 +10,20 @@ import experiment as ex
 from pulseq_assembler import PSAssembler
 st = pdb.set_trace
 
-# def asm_parse(asm_file):
-#     lines = []
-
-#     for line in asm_file:
-#         st()
-
 if __name__ == "__main__":
     lo_freq = 2.1 # MHz
-    # tx_t = 0.994 # us
     tx_t = 0.994 # us
-    # rx_t = tx_t*5 # us
-    # rx_t = 0.595
     rx_t = 0.497
     clk_t = 0.007
     grad_interval = tx_t # us between 4-channel updates
     ps = PSAssembler(rf_center=lo_freq*1e6,
                      # how many Hz the max amplitude of the RF will produce; i.e. smaller causes bigger RF V to compensate
-                     # rf_amp_max=5e3,
-                     # rf_amp_max=2e4,
-                     # rf_amp_max=1e-4,
                      rf_amp_max=16,
                      grad_max=10,
                      clk_t=clk_t,
                      tx_t=tx_t,
                      grad_t=grad_interval)
-    _, _, cb = ps.assemble('../ocra-pulseq/test_files/test5.seq')
-
+    _, _, cb = ps.assemble('../ocra-pulseq/test_files/test_loopback.seq')
 
     exp = ex.Experiment(samples=650, # TODO: get information from PSAssembler
                         lo_freq=lo_freq,
