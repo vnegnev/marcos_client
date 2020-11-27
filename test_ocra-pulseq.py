f746e8a8051830290a50742db585f75f04a2b50f
Benjamin Menkuec <benjamin@menkuec.de>
2020-11-27 23:36:28 +0100

    eliminated magic values in test_ocra-pulseq.py

--- test_ocra-pulseq.py
+++ test_ocra-pulseq.py
@@ -17,19 +17,38 @@ st = pdb.set_trace
 #         st()
 
 if __name__ == "__main__":
-    lo_freq = 2.1 # MHz
-    tx_t = 0.994 # us
-    rx_t = 0.497
-    clk_t = 0.007
-	num_grad_channels = 3	
-    grad_interval = 10 # us between [num_grad_channels] channel updates
+	lo_freq = 2.1 # MHz
+	tx_t = 0.994 # us
+	rx_t = 0.497
+	clk_t = 0.007
+	num_grad_channels = 3
+	grad_interval = 10 # us between [num_grad_channels] channel updates
+	
+	gamma = 42570000 # Hz/T
+	
+	# value for tabletopMRI  gradient coil
+	B_per_m_per_current = 0.02 # T/m/A, approximate value for tabletop gradient coil
+
+	# values for gpa fhdo
+	dac_voltage_per_current = 1/3.75 # V/A, theoretical value for gpa fhdo without 2.5 V offset!
+	max_dac_voltage = 2.5
+	
+	# values for ocra1
+	# max_dac_voltage = 5
+	# dac_voltage_per_current = 0.2 # fill in value of gradient power amplifier here!
+	
+	max_Hz_per_m = max_dac_voltage / dac_voltage_per_current * B_per_m_per_current * gamma	
+	# grad_max = max_Hz_per_m # factor used to normalize gradient amplitude, should be max value of the gpa used!	
+	grad_max = 10 # unrealistic value used only for loopback test
+	
+	rf_amp_max = 16 # factor used to normalize RF amplitude, should be max value of system used!
     ps = PSAssembler(rf_center=lo_freq*1e6,
                      # how many Hz the max amplitude of the RF will produce; i.e. smaller causes bigger RF V to compensate
                      # rf_amp_max=5e3,
                      # rf_amp_max=2e4,
                      # rf_amp_max=1e-4,
-                     rf_amp_max=16,
-                     grad_max=10,
+                     rf_amp_max=rf_amp_max,
+                     grad_max=grad_max,
                      clk_t=clk_t,
                      tx_t=tx_t,
                      grad_t=grad_interval)
