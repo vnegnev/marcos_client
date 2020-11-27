7b71080c14d1495f709118b9f11967f6a5a3d46f
Vlad Negnevitsky <vnegnev at geemail>
2020-11-27 13:26:47 +0100

    Use estimated readout samples from ocra-pulseq in test_loopback; reduce gradient update rate

--- test_loopback.py
+++ test_loopback.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     tx_t = 0.994 # us
     rx_t = 0.497
     clk_t = 0.007
-    grad_interval = tx_t # us between 4-channel updates
+    grad_interval = 4 * tx_t # us between 4-channel updates
     ps = PSAssembler(rf_center=lo_freq*1e6,
                      # how many Hz the max amplitude of the RF will produce; i.e. smaller causes bigger RF V to compensate
                      rf_amp_max=16,
@@ -23,9 +23,9 @@ if __name__ == "__main__":
                      clk_t=clk_t,
                      tx_t=tx_t,
                      grad_t=grad_interval)
-    _, _, cb = ps.assemble('../ocra-pulseq/test_files/test_loopback.seq')
+    _, _, cb, readout_samples = ps.assemble('../ocra-pulseq/test_files/test_loopback.seq')
 
-    exp = ex.Experiment(samples=650, # TODO: get information from PSAssembler
+    exp = ex.Experiment(samples=readout_samples, # TODO: get information from PSAssembler
                         lo_freq=lo_freq,
                         tx_t=tx_t,
                         rx_t=rx_t, # TODO: get information from PSAssembler
